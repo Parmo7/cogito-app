c3c4f33fd1b4705dcc4987e082585d475f672595
Parmo7 <parmi799@gmail.com>
2023-02-14 12:25:37 +0000

    Crate custom top action bar

--- app/src/main/java/uk/ac/aston/cogito/MainActivity.java
+++ app/src/main/java/uk/ac/aston/cogito/MainActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -23,14 +25,12 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.cogito_toolbar);
+        setSupportActionBar(myToolbar);
+
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_saved, R.id.navigation_history, R.id.navigation_settings)
-                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
