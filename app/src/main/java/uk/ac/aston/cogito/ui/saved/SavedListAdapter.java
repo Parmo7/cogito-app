a43c889fb5f561587f505a221b3e41dd60936046
Parmo7 <parmi799@gmail.com>
2023-02-23 17:44:24 +0000

    Implement UI for three-dots menu of config list items

--- app/src/main/java/uk/ac/aston/cogito/ui/saved/SavedListAdapter.java
+++ app/src/main/java/uk/ac/aston/cogito/ui/saved/SavedListAdapter.java
@@ -1,13 +1,18 @@
 package uk.ac.aston.cogito.ui.saved;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
@@ -15,6 +20,8 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.List;
 
 import uk.ac.aston.cogito.R;
@@ -59,6 +66,63 @@ public class SavedListAdapter extends RecyclerView.Adapter<SavedListAdapter.Save
                             .navigate(action);
                 }
             });
+
+            holder.threeDots.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    //Create a popup menu
+                    PopupMenu popup = new PopupMenu(view.getContext(), holder.threeDots);
+                    //Inflate menu from xml resource
+                    popup.inflate(R.menu.config_item_options);
+                    displayIcons(popup);
+
+
+                    //Add a click listener
+                    popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @SuppressLint("NonConstantResourceId")
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            switch (item.getItemId()) {
+                                case R.id.edit:
+                                    //handle menu1 click
+                                    Toast.makeText(view.getContext(), "EDIT", Toast.LENGTH_SHORT).show();
+                                    return true;
+                                case R.id.rename:
+                                    Toast.makeText(view.getContext(), "RENAME", Toast.LENGTH_SHORT).show();
+                                    //handle menu2 click
+                                    return true;
+                                case R.id.delete:
+                                    Toast.makeText(view.getContext(), "DELETE", Toast.LENGTH_SHORT).show();
+                                    //handle menu2 click
+                                    return true;
+                                default:
+                                    return false;
+                            }
+                        }
+                    });
+                    popup.show();
+
+                }
+            });
+        }
+
+        private void displayIcons(PopupMenu popup) {
+            try {
+                Field[] fields = popup.getClass().getDeclaredFields();
+                for (Field field : fields) {
+                    if ("mPopup".equals(field.getName())) {
+                        field.setAccessible(true);
+                        Object menuPopupHelper = field.get(popup);
+                        Class<?> classPopupHelper = Class.forName(menuPopupHelper.getClass().getName());
+                        Method setForceIcons = classPopupHelper.getMethod("setForceShowIcon", boolean.class);
+                        setForceIcons.invoke(menuPopupHelper, true);
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
@@ -79,6 +143,7 @@ public class SavedListAdapter extends RecyclerView.Adapter<SavedListAdapter.Save
             public final TextView nameTextView;
             public final TextView durationTextView;
             public final FrameLayout playBtn;
+            public final ImageView threeDots;
 
             public SavedViewHolder(@NonNull View itemView, SavedListAdapter adapter) {
                 super(itemView);
@@ -86,6 +151,7 @@ public class SavedListAdapter extends RecyclerView.Adapter<SavedListAdapter.Save
                 nameTextView = itemView.findViewById(R.id.config_item_name);
                 durationTextView = itemView.findViewById(R.id.config_item_duration);
                 playBtn = itemView.findViewById(R.id.play_btn);
+                threeDots = itemView.findViewById(R.id.three_dots);
 
                 this.adapter = adapter;
             }
