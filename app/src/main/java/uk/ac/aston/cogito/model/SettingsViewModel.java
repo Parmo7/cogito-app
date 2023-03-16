3ee50132bb765f79adcaac2edd643821a3faa6ef
Parmo7 <parmi799@gmail.com>
2023-03-16 22:37:28 +0000

    Build UI for Settings page

---
+++ app/src/main/java/uk/ac/aston/cogito/model/SettingsViewModel.java
@@ -0,0 +1,10 @@
+package uk.ac.aston.cogito.model;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SettingsViewModel extends ViewModel {
+
+
+}
--- app/src/main/java/uk/ac/aston/cogito/ui/settings/SettingsFragment.java
+++ app/src/main/java/uk/ac/aston/cogito/ui/settings/SettingsFragment.java
@@ -3,6 +3,7 @@ package uk.ac.aston.cogito.ui.settings;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -11,29 +12,43 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import uk.ac.aston.cogito.R;
 import uk.ac.aston.cogito.databinding.FragmentSettingsBinding;
+import uk.ac.aston.cogito.model.SettingsViewModel;
+import uk.ac.aston.cogito.ui.dialogs.BottomDialogListener;
+import uk.ac.aston.cogito.ui.dialogs.FormBottomDialog;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements BottomDialogListener {
 
     private FragmentSettingsBinding binding;
+    private SettingsViewModel model;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        SettingsViewModel settingsViewModel =
-                new ViewModelProvider(this).get(SettingsViewModel.class);
-
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        model = new ViewModelProvider(this).get(SettingsViewModel.class);
 
-        final TextView textView = binding.textSettings;
-        settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return binding.getRoot();
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+
         binding = null;
+    }
+
+
+    @Override
+    public void onDoneBtnPressed(FormBottomDialog dialog) {
+
     }
 }
